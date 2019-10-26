ed59d4eefdf2d096790ceeee111f4c07dece5de6
pdual <pablo.du.alv@gmail.com>
2019-10-26 17:17:32 +0200

    test2

--- src/paquete/cliente_test.java
+++ src/paquete/cliente_test.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//Pepe ha escrito aqui
+//Pepe ha escrito aqui y paco tambien 
 package paquete;
 
 import java.io.BufferedReader;
